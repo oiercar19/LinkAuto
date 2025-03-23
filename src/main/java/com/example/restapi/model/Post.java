62e196e6017ee99bb3864ef5a3cd7538ff62dea4
Mega <Mega@PC-DE-LA-NASA>
2025-03-23 13:22:03 +0100

    feat: add domain class

---
+++ src/main/java/com/example/restapi/model/Post.java
@@ -0,0 +1,73 @@
+package com.example.restapi.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Post {
+    private Long id;             
+    private String usuario;      
+    private String mensaje;      
+    private long fechaCreacion;  
+    private List<String> imagenes; 
+
+    public Post() {
+        this.imagenes = new ArrayList<>();
+    }
+
+    public Post(Long id, String usuario, String mensaje, long fechaCreacion, List<String> imagenes) {
+        this.id = id;
+        this.usuario = usuario;
+        this.mensaje = mensaje;
+        this.fechaCreacion = fechaCreacion;
+        // Se utiliza la lista proporcionada o se inicializa una nueva si es nula
+        this.imagenes = (imagenes != null) ? imagenes : new ArrayList<>();
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public long getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(long fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public List<String> getImagenes() {
+        return imagenes;
+    }
+
+    public void setImagenes(List<String> imagenes) {
+        this.imagenes = imagenes;
+    }
+
+    @Override
+    public String toString() {
+        return "Post [id=" + id + ", usuario=" + usuario + ", mensaje=" + mensaje 
+                + ", fechaCreacion=" + fechaCreacion + ", imagenes=" + imagenes + "]";
+    }
+}
+
