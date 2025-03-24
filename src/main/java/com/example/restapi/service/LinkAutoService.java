b554dcd7dd7a112fe11a1c78d9e040e33138bcf5
Imanol Dominguez <ima.dominguez@opendeusto.es>
2025-03-24 12:36:30 +0100

    service methods for user class

--- src/main/java/com/example/restapi/service/LinkAutoService.java
+++ src/main/java/com/example/restapi/service/LinkAutoService.java
@@ -49,4 +49,39 @@ public class LinkAutoService {
             throw new RuntimeException("Book not found with id: " + id);
         }
     }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User updateUser(String username, User userDetails) {
+        Optional<User> optionalUser = userRepository.findById(username);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            user.setName(userDetails.getName());
+            user.setProfilePicture(userDetails.getProfilePicture());
+            user.setEmail(userDetails.getEmail());
+            user.setCars(userDetails.getCars());
+            user.setBirthDate(userDetails.getBirthDate());
+            user.setGender(userDetails.getGender());
+            user.setLocation(userDetails.getLocation());
+            user.setPassword(userDetails.getPassword());
+            user.setDescription(userDetails.getDescription());
+            return userRepository.save(user);
+        } else {
+            throw new RuntimeException("User not found");
+        }
+    }
+
+    public void deleteUser(String username) {
+        if (userRepository.existsById(username)) {
+            userRepository.deleteById(username);
+        } else {
+            throw new RuntimeException("User not found with username: " + username);
+        }
+    }
 }
