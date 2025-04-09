1ae48146e01dc76be24de9d4c75ecd87d533abbf
Oier Carbón <oier.carbon@opendeusto.es>
2025-04-09 10:43:20 +0200

    feat: Add CommentReturnerDTO class for returning comment data

---
+++ rest-api/src/main/java/com/linkauto/restapi/dto/CommentReturnerDTO.java
@@ -0,0 +1,61 @@
+package com.linkauto.restapi.dto;
+
+public class CommentReturnerDTO {
+
+    private Long id;
+    private String text;
+    private String username;
+    private Long post_id;
+
+    public CommentReturnerDTO() {
+    }
+
+    public CommentReturnerDTO(Long id, String text, String username, Long post_id) {
+        this.id = id;
+        this.text = text;
+        this.username = username;
+        this.post_id = post_id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Long getPost_id() {
+        return post_id;
+    }
+
+    public void setPost_id(Long post_id) {
+        this.post_id = post_id;
+    }
+
+    @Override
+    public String toString() {
+        return "CommentReturner{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                ", username='" + username + '\'' +
+                ", post_id='" + post_id + '\'' +
+                '}';
+    }
+}
