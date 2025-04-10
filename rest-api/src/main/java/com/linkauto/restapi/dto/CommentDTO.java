632157ccf0ed74564ff311ab54ccd9163ef0ab00
Oier Carbón <oier.carbon@opendeusto.es>
2025-04-10 20:31:36 +0200

    refactor: remove creationDate from CommentDTO and set current time in LinkAutoService

--- rest-api/src/main/java/com/linkauto/restapi/dto/CommentDTO.java
+++ rest-api/src/main/java/com/linkauto/restapi/dto/CommentDTO.java
@@ -2,14 +2,12 @@ package com.linkauto.restapi.dto;
 
 public class CommentDTO {
     private String text;
-    private Long creationDate;
 
     public CommentDTO() {
     }
 
-    public CommentDTO(String text, Long creationDate) {
+    public CommentDTO(String text) {
         this.text = text;
-        this.creationDate = creationDate;
     }
 
     public String getText() {
@@ -20,19 +18,10 @@ public class CommentDTO {
         this.text = text;
     }
 
-    public Long getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(Long creationDate) {
-        this.creationDate = creationDate;
-    }
-
     @Override
     public String toString() {
         return "CommentDTO{" +
                 "text='" + text + '\'' +
-                ", fecha=" + creationDate +
                 '}';
     }
 
