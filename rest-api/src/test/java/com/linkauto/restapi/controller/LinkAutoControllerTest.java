a84122b49bd68cab8b3903422094d7009e43b447
Adrian Fernandez <adrian.f@opendeusto.es>
2025-04-18 17:06:09 +0200

    test: create post

--- rest-api/src/test/java/com/linkauto/restapi/controller/LinkAutoControllerTest.java
+++ rest-api/src/test/java/com/linkauto/restapi/controller/LinkAutoControllerTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +17,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 
+import com.linkauto.restapi.dto.PostDTO;
 import com.linkauto.restapi.dto.PostReturnerDTO;
 import com.linkauto.restapi.model.Comment;
 import com.linkauto.restapi.model.Post;
@@ -29,26 +31,27 @@ public class LinkAutoControllerTest {
     LinkAutoService linkAutoService;
     AuthService authService;
     LinkAutoController linkAutoController;
+    User usuario;
 
     @BeforeEach
     public void setUp() {
         linkAutoService = mock(LinkAutoService.class);
         authService = mock(AuthService.class);
         linkAutoController = new LinkAutoController(linkAutoService, authService);
+        usuario = new User("ownerUsername", "ownerName", "ownerProfilePicture", "ownerEmail", new ArrayList<>(), 123456L, Gender.MALE, "ownerLocation", "ownerPassword", "ownerDescription", new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
     }
     
     @Test
     public void testGetAllPosts() {
-        User usuarioPropietario = new User("ownerUsername", "ownerName", "ownerProfilePicture", "ownerEmail", new ArrayList<>(), 123456L, Gender.MALE, "ownerLocation", "ownerPassword", "ownerDescription", new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
         List<Post> posts = new ArrayList<>();
 
-        Post post = new Post(1L, usuarioPropietario, "testMessage", 1234567, new ArrayList<>(), new ArrayList<>(), new HashSet<>());
-        Comment comment = new Comment("testComment", usuarioPropietario, post, 9999L);
-        Comment comment2 = new Comment("testComment2", usuarioPropietario, post, 9999L);
+        Post post = new Post(1L, usuario, "testMessage", 1234567, new ArrayList<>(), new ArrayList<>(), new HashSet<>());
+        Comment comment = new Comment("testComment", usuario, post, 9999L);
+        Comment comment2 = new Comment("testComment2", usuario, post, 9999L);
         post.addComentario(comment);
         post.addComentario(comment2);
         
-        Post post2 = new Post(2L, usuarioPropietario, "testMessage", 1234567, new ArrayList<>(), new ArrayList<>(), new HashSet<>());
+        Post post2 = new Post(2L, usuario, "testMessage", 1234567, new ArrayList<>(), new ArrayList<>(), new HashSet<>());
         posts.add(post);
         posts.add(post2);
         when(linkAutoService.getAllPosts()).thenReturn(posts);
@@ -62,8 +65,7 @@ public class LinkAutoControllerTest {
 
     @Test
     public void testGetPostById() {
-        User usuarioPropietario = new User("ownerUsername", "ownerName", "ownerProfilePicture", "ownerEmail", new ArrayList<>(), 123456L, Gender.MALE, "ownerLocation", "ownerPassword", "ownerDescription", new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
-        Post post = new Post(1L, usuarioPropietario, "testMessage", 1234567, new ArrayList<>(), new ArrayList<>(), new HashSet<>());
+        Post post = new Post(1L, usuario, "testMessage", 1234567, new ArrayList<>(), new ArrayList<>(), new HashSet<>());
         when(linkAutoService.getPostById(1L)).thenReturn(Optional.of(post));
         ResponseEntity<Post> result = linkAutoController.getPostById(1L);
         assertEquals(HttpStatus.OK, result.getStatusCode());
@@ -75,5 +77,26 @@ public class LinkAutoControllerTest {
         assertEquals(HttpStatus.NOT_FOUND, result2.getStatusCode());
     }
 
-    
+    @Test
+    public void createPost(){
+        String userToken = "1234";
+
+        when(authService.isTokenValid(userToken)).thenReturn(false);
+        ResponseEntity<PostReturnerDTO> result = linkAutoController.createPost(userToken, new PostDTO("testPost", Arrays.asList("img1", "img2")));
+        assertEquals(HttpStatus.UNAUTHORIZED, result.getStatusCode());
+
+        when(authService.isTokenValid(userToken)).thenReturn(true);
+        when(authService.getUserByToken(userToken)).thenReturn(usuario);
+        Post post = new Post(1L, usuario, "testPost", 1234567, Arrays.asList("img1", "img2"), new ArrayList<>(), new HashSet<>());
+        PostDTO postDTO = new PostDTO("testPost", Arrays.asList("img1", "img2"));
+        when(linkAutoService.createPost(postDTO, usuario)).thenReturn(post);
+        ResponseEntity<PostReturnerDTO> result2 = linkAutoController.createPost(userToken, postDTO);
+        assertEquals(HttpStatus.OK, result2.getStatusCode());
+        assertNotNull(result2.getBody());
+        assertEquals("testPost", result2.getBody().getMessage());
+        assertEquals(1L, result2.getBody().getId());
+        assertEquals(1234567, result2.getBody().getCreationDate());
+        assertEquals(Arrays.asList("img1", "img2"), result2.getBody().getImages());
+    }
+
 }
