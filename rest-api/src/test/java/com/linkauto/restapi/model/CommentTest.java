6dadabe1160ea742863c8caa98679495b307044f
Alejandro Martinez <alejandro.martinez@opendeusto.es>
2025-04-21 18:22:35 +0200

    refactor: comment out unused import statements in Comment and CommentTest classes

--- rest-api/src/test/java/com/linkauto/restapi/model/CommentTest.java
+++ rest-api/src/test/java/com/linkauto/restapi/model/CommentTest.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
+//import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
